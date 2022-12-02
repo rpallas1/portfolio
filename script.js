9ce1fe423be0895806b7afd16e2ab82ab9d18499
Rpallas1 <rpallas1@asu.edu>
2022-12-02 09:53:23 -0700

    added project pages and more styling

---
+++ script.js
@@ -0,0 +1,4 @@
+// TODO: when featured buttons are pressed, shift the tiles and change the link destination and the h3 for each project
+/* When click right button bring the right image to the main and move main to left and left to right
+  - store the ones moving and then place them in the correct div with a certain id
+*/
